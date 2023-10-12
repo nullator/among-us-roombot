a3484e7a8e91fc33ac87dd6659f807e9ba480292
nullator <diver.rus@gmail.com>
2023-10-12 22:10:18 +0400

    Добавил админское удаление комнаты

--- internals/service/telegram/delete.go
+++ internals/service/telegram/delete.go
@@ -3,12 +3,48 @@ package telegram
 import (
 	"fmt"
 	"log/slog"
+	"os"
+	"regexp"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func (b *Telegram) handleDel(message *tgbotapi.Message) error {
 	const path = "service.telegram.delete.handleDel"
+
+	arg := message.CommandArguments()
+	slog.Debug("Получены аргументы команды del: %s", arg)
+	if arg != "" {
+		match, _ := regexp.MatchString("^[a-zA-Z]{6}$", arg)
+		if !match {
+			return nil
+		}
+		code := strings.ToUpper(arg)
+
+		admin_list := os.Getenv("ADMINS")
+		admins := strings.Split(admin_list, ",")
+		if b.isAdmin(admins, fmt.Sprint(message.Chat.ID)) {
+			err := b.rep.DeleteRoom(code)
+			if err != nil {
+				slog.Error("Ошибка удаления комнаты из БД")
+				return fmt.Errorf("%s: %w", path, err)
+			}
+			msg_text := fmt.Sprintf("Комната %s удалена", code)
+			msg := tgbotapi.NewMessage(message.Chat.ID, msg_text)
+			_, err = b.bot.Send(msg)
+			if err != nil {
+				slog.Error("error send message to user")
+				return fmt.Errorf("%s: %w", path, err)
+			}
+			slog.Info("Комната удалена администратором",
+				slog.String("code", code),
+				slog.String("admin", fmt.Sprint(message.Chat.ID)),
+				slog.String("username", message.Chat.UserName))
+
+			return nil
+		}
+	}
 
 	exist_room, err := b.rep.GetUserStatus(message.Chat.ID, "room")
 	if err != nil {
@@ -82,4 +118,13 @@ func (b *Telegram) delete(message *tgbotapi.Message) error {
 
 	return nil
 
+}
+
+func (b *Telegram) isAdmin(list []string, id string) bool {
+	for _, v := range list {
+		if v == id {
+			return true
+		}
+	}
+	return false
 }
